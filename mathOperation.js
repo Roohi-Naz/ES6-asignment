6904dc2246ce9aae9847121a9e6fb20cf3152be5
Roohi-Naz <roohinaz700@gmail.com>
2024-11-27 23:08:14 +0500

    Initiallizing assignment

---
+++ mathOperation.js
@@ -0,0 +1,9 @@
+// mathOperations.js
+export function operation(a, b) {
+    let add = a + b;
+    let subt = a - b;
+    let multiply = a * b;
+    let divide = a / b;
+    console.log(`Add: ${add}, Subtract: ${subt}, Multiply: ${multiply}, Divide: ${divide}`);
+}
+
